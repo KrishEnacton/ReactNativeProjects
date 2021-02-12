50fe9637c9b5f49aaa4ca58bd50b4b5bd2d22d7a
Krish Kapadia <kpkapadia@enacton.com>
2021-02-12 16:04:56 +0530

    done

--- ReduxFirebaseApp/components/HomeComponent.js
+++ ReduxFirebaseApp/components/HomeComponent.js
@@ -2,16 +2,21 @@ import React from 'react';
 import {
     StyleSheet,
     Button,
-    View
+    View,
+    ToastAndroid
 } from 'react-native';
 
 const HomeComponent = ({ navigation }) => {
+    const tN = () => {
+        ToastAndroid.show("Hello", ToastAndroid.LONG);
+    }
     return (
         <>
             <View style={{ marginTop: "50%" }}>
                 <Button title="Redux Saga" onPress={() => navigation.navigate('Redux')} />
                 <Button title="Firebase" onPress={() => navigation.navigate('Firebase')} />
                 <Button title="Webview" onPress={() => navigation.navigate('Webview')} />
+                <Button title="Toast Notification" onPress={() => tN()} />
             </View>
         </>
     );
